33141f9d0898a1c367b8b650dc91b68a15a0f154
IVAN TECPANECATL MARTINEZ <ivan.tecpanecatl@elektra.com.mx>
2023-01-03 18:49:05 -0600

    primer commit, proyecto configurado para compilar sin storyboard

---
+++ calculatoruikit/ui/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculatoruikit
+//
+//  Created by Ivan Tecpanecatl Martinez on 03/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
